1cb73d460dff5636096e410b7a05028662a3422c
Subayyal Saeed <subayyalsaeed321@gmail.com>
2024-08-15 22:29:10 +0500

    inital

---
+++ app/forms.py
@@ -0,0 +1,62 @@
+# Using Flask WTF (WT Forms) to represent web forms
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DecimalField, RadioField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User, Teacher
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Name', validators=[DataRequired()])
+    # Automatic resolving of email
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    dept = SelectField('Department', choices=[(
+        'CSE', 'CSE'), ('IT', 'IT'), ('Software', 'Software')], validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    # Check if both passwords are the same
+    password2 = PasswordField('Repeat Password', validators=[
+                              DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # validate_{field_name} makes a custom validator automatically used for that field
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('E-mail is already in use. Try again.')
+        domain = email.data.split('@')[1]
+        if(domain != 'lhr.nu.edu.pk'):
+            raise ValidationError('Please enter a FAST-LHR NU email ID')
+
+
+class AddTeacherForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    dept = SelectField('Department', choices=[(
+        'CSE', 'CSE'), ('IT', 'IT'), ('Software', 'Software')], validators=[DataRequired()])
+    submit = SubmitField('Add Teacher')
+
+
+class RateTeacherForm(FlaskForm):
+    dedication_score = RadioField('Dedication', choices=[(
+        '1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+    leniency_score = RadioField('Leniency', choices=[(
+        '1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+    marks_score = RadioField('Marking', choices=[(
+        '1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+    teaching_score = RadioField('Teaching', choices=[(
+        '1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+    friendliness_score = RadioField('Friendliness', choices=[(
+        '1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+    submit = SubmitField('Submit Rating')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Post Comment')
---
+++ app/routes.py
@@ -0,0 +1,168 @@
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, AddTeacherForm, RateTeacherForm, CommentForm
+from flask import render_template, redirect, flash, make_response, jsonify, url_for, request
+from flask_httpauth import HTTPBasicAuth
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Teacher, Rating, Comment
+from werkzeug.urls import url_parse
+from datetime import datetime
+from .views import update_score
+
+
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    if(username == "sid"):
+        return 'admin'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}, 403))
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title="Sign In", form=form)
+
+
+@app.route('/add_teacher', methods=['GET', 'POST'])
+@login_required
+def add_teacher():
+    form = AddTeacherForm()
+
+    if form.validate_on_submit():
+        teacher = Teacher.query.filter_by(
+            first_name=form.first_name.data, last_name=form.last_name.data, dept=form.dept.data).first()
+        if teacher is not None:
+            flash('Teacher already exists')
+            return redirect(url_for('add_teacher'))
+        teacher = Teacher(first_name=form.first_name.data,
+                          last_name=form.last_name.data, dept=form.dept.data, created_by=current_user.id)
+        db.session.add(teacher)
+        db.session.commit()
+        flash("Successfully Added New Teacher!")
+        return redirect(url_for('add_teacher'))
+    return render_template('add_teacher.html', title='Add Teacher', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Successfully Registered!")
+        return redirect(url_for('login'))
+    # Title is going to base.html through register.html
+    return render_template('register.html', title="Register", form=form)
+
+
+@app.route('/view_teachers')
+@login_required
+def view_teachers():
+    teachers = Teacher.query.order_by(Teacher.overall_score.desc()).all()
+    if(len(teachers)):
+        return render_template('view_teachers.html', title="Teachers", teachers=teachers)
+    flash('No teachers added. Please add some.')
+    return redirect(url_for('add_teacher'))
+
+
+@app.route('/rate_teacher/<teacherId>', methods=['GET', 'POST'])
+@login_required
+def rate_teacher(teacherId):
+    user_id = current_user.id
+    rating = Rating.query.filter_by(
+        user_id=user_id, teacher_id=teacherId).first()
+    if rating is not None:
+        flash('You have already rated this teacher!')
+        return redirect(url_for('view_teachers'))
+    form = RateTeacherForm()
+    if form.validate_on_submit():
+        dedication_score = form.dedication_score.data
+        leniency_score = form.leniency_score.data
+        marks_score = form.marks_score.data
+        teaching_score = form.teaching_score.data
+        friendliness_score = form.friendliness_score.data
+        rating = Rating(teacher_id=teacherId, user_id=current_user.id,
+                        dedication_score=dedication_score,
+                        leniency_score=leniency_score,
+                        marks_score=marks_score,
+                        teaching_score=teaching_score,
+                        friendliness_score=friendliness_score)
+
+        if(update_score(teacherId, dedication_score, leniency_score, marks_score, teaching_score, friendliness_score)):
+            db.session.add(rating)
+            db.session.commit()
+            flash('Successfuly rated! Thank you for your contribution.')
+            return redirect(url_for('view_teachers'))
+        else:
+            flash(
+                'Unknown error. Sorry, please report the issue to the admin. Try again.')
+            return redirect(url_for(rate_teacher, teacherId=teacherId))
+    teacher = Teacher.query.filter_by(id=teacherId).first()
+    return render_template('rate_teacher.html', title="Rate Teacher", teacher=teacher, form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("Logged out Successfully.")
+    return redirect(url_for('login'))
+
+
+@app.route('/teacher/<teacherId>', methods=['GET', 'POST'])
+@login_required
+def teacher(teacherId):
+    teacher = Teacher.query.filter_by(id=teacherId).first()
+
+    if teacher is not None:
+        # Checking if user has previously commented on this teacher.
+        prevcomment = Comment.query.filter_by(user_id=current_user.id, teacher_id=teacherId).first()
+        print(prevcomment)
+        if prevcomment is None:
+            form = CommentForm()
+            if form.validate_on_submit():
+                comment = Comment(teacher_id=teacherId,
+                                  user_id=current_user.id, value=form.comment.data)
+                db.session.add(comment)
+                db.session.commit()
+                flash('Successfully added comment!')
+                comments = Comment.query.filter_by(teacher_id=teacherId).all()
+                return redirect(url_for('teacher', teacherId=teacherId))
+                # return render_template('teacher.html', teacher=teacher, comments=comments)
+
+            comments = Comment.query.filter_by(teacher_id=teacherId).all()
+            return render_template('teacher.html', teacher=teacher, form=form, comments=comments)
+        comments = Comment.query.filter_by(teacher_id=teacherId).all()
+        return render_template('teacher.html', teacher=teacher, comments=comments)
+    flash('Invalid Teacher ID')
+
+    return render_template(url_for('view_teachers'))
